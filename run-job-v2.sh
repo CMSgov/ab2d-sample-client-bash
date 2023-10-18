e77a6df5757de00becf9406cacd0334a437f5c5b
Sean Fern <seanfern@navapbc.com>
2023-10-18 15:55:55 -0400

    Output response body from patient endpoint for parsing job json

--- run-job-v2.sh
+++ run-job-v2.sh
@@ -32,9 +32,8 @@ fi
 
 echo "Attempting to start job using $URL"
 
-PATIENT_HEADERS_FILE="$DIRECTORY/patient_response_headers.txt"
+PATIENT_HEADERS_FILE="$DIRECTORY/patient_headers.txt"
 HTTP_CODE=$(curl "$URL" \
-    -v \
     -s \
     -w "%{http_code}" \
     -D "$PATIENT_HEADERS_FILE" \
@@ -57,60 +56,63 @@ else
       echo "Could not parse response for job id. Make sure to save the job id located on the line with 'content-location'"
       exit 1
     fi
-
-    echo "$JOB created"
 fi
 
 # ------ Monitoring job
 echo "Monitoring job with job id $JOB"
 
-URLS_HEADER=""
-JOB_HEADERS_FILE="$DIRECTORY/job_response_headers.txt"
+JOB_HEADERS_FILE="$DIRECTORY/job_headers.txt"
+JOB_RESPONSE_FILE="$DIRECTORY/job_response.txt"
+
+# Empty response file to avoid getting URLs from a previous run
+echo -n "" > "$JOB_RESPONSE_FILE"
+
+JOB_JSON=""
 COUNTER=0
 
-while [ "$URLS_HEADER" == '' ]
-do
+while [ "$JOB_JSON" == '' ]; do
     # Sleep and increment counter
     sleep 60
     COUNTER=$(( COUNTER +1 ))
     echo "Running for $COUNTER minutes"
 
     HTTP_CODE=$(curl "${API_URL}/Job/${JOB}/\$status" \
-        -v \
         -s \
         -w "%{http_code}" \
         -D "$JOB_HEADERS_FILE" \
+        -o "$JOB_RESPONSE_FILE" \
         -H "accept: application/json" \
         -H "Authorization: Bearer ${BEARER_TOKEN}")
 
     cat "$JOB_HEADERS_FILE"
+    cat "$JOB_RESPONSE_FILE"
 
     if [ "$HTTP_CODE" == 403 ]; then
         # If response is unauthorized refresh token and try again
         echo "Token expired. Refreshing and then attempting to check status again"
-        BEARER_TOKEN=$(fn_get_token "$IDP_URL" "$AUTH_FILE")
+        BEARER_TOKEN="$(fn_get_token "$IDP_URL" "$AUTH_FILE")"
     elif [ "$HTTP_CODE" != 202 ] && [ "$HTTP_CODE" != 200 ]; then
         echo "Error making rest call. Status code: $HTTP_CODE"
         exit 1
     else
-        URLS_HEADER="$(grep ExplanationOfBenefit "$JOB_HEADERS_FILE")"
+        JOB_JSON="$(grep ExplanationOfBenefit "$JOB_RESPONSE_FILE")"
     fi
 done
 
-echo "Saved response to $JOB_HEADERS_FILE"
+echo "Saved response to $JOB_RESPONSE_FILE"
 
 # ------ Download results for job
 echo "Downloading results for job"
 
-URLS=$(echo "$URLS_HEADER" | jq --raw-output ".output[].url")
+URLS="$(echo "$JOB_JSON" | jq --raw-output ".output[].url")"
 echo "List of files to download: $URLS"
 FILE_DOWNLOAD_HEADERS="$DIRECTORY/file_download_headers.txt"
 COUNTER=0
 
 for URL in $URLS; do
-    FILE_NAME="$DIRECTORY"/$(echo ${URL} | sed 's/.*.file.//')
+    FILE_NAME="$DIRECTORY/$(echo "$URL" | sed 's/.*.file.//')"
 
-    echo "Downloading file from $URL"
+    echo "Downloading file to $FILE_NAME from $URL"
 
     if [ -f "$FILE_NAME" ]; then
         echo "$FILE_NAME already exists, skipping"
@@ -126,7 +128,7 @@ for URL in $URLS; do
             if [ "$HTTP_CODE" == 403 ]; then
                 # If response is unauthorized refresh token and try again
                 echo "Bearer token expired. Refreshing, then attempting to download again"
-                BEARER_TOKEN=$(fn_get_token "$IDP_URL" "$AUTH_FILE")
+                BEARER_TOKEN="$(fn_get_token "$IDP_URL" "$AUTH_FILE")"
             elif [ "$HTTP_CODE" != 200 ]; then
                 echo "Error downloading file. Status code: $HTTP_CODE"
                 cat "$FILE_DOWNLOAD_HEADERS"
